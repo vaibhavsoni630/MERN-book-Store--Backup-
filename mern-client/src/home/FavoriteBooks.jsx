fd4bf733be9e83d95134625ed29ff74ef8098efe
vaibhavsoni630 <125847667+vaibhavsoni630@users.noreply.github.com>
2023-12-16 13:26:38 +0530

    Update FavoriteBooks.jsx

--- mern-client/src/home/FavoriteBooks.jsx
+++ mern-client/src/home/FavoriteBooks.jsx
@@ -8,7 +8,7 @@ const FavoriteBooks = () => {
     const [books ,setBooks] = useState([])
 
     useEffect(()=>{
-        fetch("http://localhost:3000/all-books").then(res => res.json()).then(data => setBooks(data.slice(0,9)))
+        fetch("https://bookbackend44.onrender.com/all-books").then(res => res.json()).then(data => setBooks(data.slice(0,9)))
     },[])
   return (
     <div>
